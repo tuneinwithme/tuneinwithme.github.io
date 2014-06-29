7074c89d285e6cd5ea0f49a397af06720de95a60
Sean Zhu <interestinglythere@gmail.com>
2014-06-29 12:27:22 -0700

    Server fixes

--- server.py
+++ server.py
@@ -3,25 +3,44 @@
 #!/usr/bin/env python
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
+HOST = 'localhost'
+PORT = 63298
+
 
 class TuneinwithmeHandler(SimpleHTTPRequestHandler):
+
+    # Incorrect but this keeps Chrome from complaining
+    extensions_map = SimpleHTTPRequestHandler.extensions_map.copy()
+    extensions_map.update({'.coffee': 'text/javascript'})
+
     # @property
     # def error_message_format(self):
     #     with file('404.html', 'r') as f:
     #         return f.read()
 
-    def translate_path(self, path):
-        if path.startswith('/assets'):
-            return path[1:]
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def do_GET(self):
+        from urlparse import urlparse
+        from os import access, R_OK
+        urlParams = urlparse(self.path)
+        if access('./' + urlParams.path, R_OK):
+            SimpleHTTPRequestHandler.do_GET(self)
         else:
-            return 'views/room.html'
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(open('views/room.html').read())
 
 
 def server():
     from BaseHTTPServer import HTTPServer
 
     try:
-        httpd = HTTPServer(('localhost', 63298), TuneinwithmeHandler)
+        httpd = HTTPServer((HOST, PORT), TuneinwithmeHandler)
+        print "Serving tuneinwithme on http://%s:%s ..." % (HOST, PORT)
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
